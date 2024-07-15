7f25505989aafda24480a645deee50f06368343c
tinaliu27 <27tinaliu@gmail.com>
2024-07-14 23:29:06 -0700

    changed n and m

--- lab1/lab1.java
+++ lab1/lab1.java
@@ -15,9 +15,11 @@ public class lab1 {
     public static void main(String [] args) {
         lab1 lab = new lab1();
         lab.readNumbers(NUMBERSIN);
+        // we assumed that there would a random number of n rows along with a random number of groups because  it was unclear what the parameters were supposed to be for n and m respectively. 
+        // The code also runs if you initalize n_groups and m to be fixed values  ie.  n_groups = 3 and m = 67
         int n_groups = lab.randomGenerator(1, 20);
-        int m = lab.randomGenerator(1, 10);
-        lab.writeNumbers(n_groups, m, NUMBERSOUT); 
+        int m = lab.randomGenerator(1, 20);
+        lab.writeNumbers(m, n_groups, NUMBERSOUT); 
     } public void readNumbers(String file_name) {
         try (BufferedReader br = new BufferedReader(new FileReader(file_name))) {
             String current_number;
@@ -33,14 +35,14 @@ public class lab1 {
         try (BufferedWriter  br = new BufferedWriter(new FileWriter(file_name, false))) {
             // header 
             br.write("partNr;");
-            for(int p = 1; p <= m; p++) {
+            for(int p = 1; p <= n; p++) {
                 br.write("f"+p+";");
             }
             br.newLine();
             // content 
-            for(int i = 1; i <= n; i++) {
+            for(int i = 1; i <= m; i++) {
                 br.write(i+";");
-                for(int x = 0; x< m; x++) {
+                for(int x = 0; x< n; x++) {
                     int randomIndex = randomGenerator(0, official_list_of_numbers.size()-1);
                     br.write(official_list_of_numbers.get(randomIndex));
                 }
